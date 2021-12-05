d7ee4c1cbd44d18d9e3354ba792b1ef2cbc008c8
yqs <1448334428@qq.com>
2021-12-05 20:10:58 +0800

    无

--- 笔记/python的库/2、字符串的五种加密方式.py
+++ 笔记/python的库/2、字符串的五种加密方式.py
@@ -21,4 +21,31 @@ python中对字符串的 五种加密方式以及代码
 # print(e)
 # print(c)
 """三、 字符串转换ascii"""
+# name = "王大"
+# # 编码
+# ascii_name = list(map(ord, name))
+# # print(ascii_name)
+# # 解码
+# l_name = "".join(map(chr, ascii_name))
+# print(l_name)
 
+"""四、md5 不可逆"""
+import hashlib
+
+name = '123456'
+l_md5 = hashlib.md5(name.encode()).hexdigest()
+print(l_md5)
+
+"""五、 Unicode 转中文"""
+name = "王大锤"
+# 编码
+unicode_name = name.encode('unicode_escape')
+utf8_name = name.encode("utf-8")
+gbk_name = name.encode('gbk')
+gb2312_name = name.encode("gb2312")
+
+# 解码
+l_unci = unicode_name.decode('unicode_escape')
+l_utf8_name = utf8_name.decode()  # 默认是utf-8   括号内可以带utf-8
+l_gbk_name = gb2312_name.decode('gbk')  # gb2312 属于gbk的一种
+print(l_gbk_name)
